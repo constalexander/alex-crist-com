a4567084df01880b31dca01213562a2981d54e59
Alex Crist <alex@alex-crist.com>
2024-06-08 16:18:02 -0500

    shadcn

---
+++ libs/ui/src/lib/ui.tsx
@@ -0,0 +1,14 @@
+import './ui.scss';
+
+/* eslint-disable-next-line */
+export interface UIProps {}
+
+export function UI(props: UIProps) {
+  return (
+    <div>
+      <h1>UI</h1>
+    </div>
+  );
+}
+
+export default UI;
