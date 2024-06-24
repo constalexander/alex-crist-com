2879657923e8b7a832f7eec220ed141cbd7d6d0d
Alex Crist <alex@alex-crist.com>
2024-06-23 22:00:02 -0500

    libs

---
+++ libs/guards/src/lib/guards.spec.tsx
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react';
+
+import Guards from './guards';
+
+describe('Guards', () => {
+  it('should render successfully', () => {
+    const { baseElement } = render(<Guards />);
+    expect(baseElement).toBeTruthy();
+  });
+});
