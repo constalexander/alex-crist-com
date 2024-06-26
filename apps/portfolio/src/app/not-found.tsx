fc1d347caa8ab391fadbb4ef2e0b4a2e0dacf958
Alex Crist <alex@alex-crist.com>
2024-06-26 03:50:21 -0500

    .

--- apps/portfolio/src/app/not-found.tsx
+++ apps/portfolio/src/app/not-found.tsx
@@ -2,12 +2,12 @@ import { Button } from '@alex/ui/atoms/button';
 
 export default function Page() {
   return (
-    <div>
-      <h1 className="text-[10vw] text-slate-400 ml-[10px] mt-[-20px]">
-        Alex Crist
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-6xl text-zinc-700 ml-[10px] tracking-wide">
+        404 NOT FOUND
       </h1>
       <Button variant="outline" className="mt-5">
-        Send it
+        Go Home
       </Button>
     </div>
   );
