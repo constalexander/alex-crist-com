97fc65bf1cd2c65bdd3f5d6bf432b0db11e931d3
Alex Crist <alex@alex-crist.com>
2024-07-17 22:15:10 -0500

    .

--- apps/portfolio/src/app/page.tsx
+++ apps/portfolio/src/app/page.tsx
@@ -4,7 +4,11 @@ import { EmploymentExperience } from '@alex/ui/organisms/employment-experience';
 
 export default function Page() {
   return (
-    <div className="p-2">
+    <div className="">
+      <p className="text-stone-200 leading-5 pb-8">
+        Versatile and innovative software developer creating bespoke user
+        interfaces. leveraging modern technologies and practices.
+      </p>
       <EmploymentExperience />
     </div>
   );
