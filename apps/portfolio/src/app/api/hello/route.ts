2027a9c885dac67eb160ccdc2d3a78f0d409b8fc
Alex Crist <alex@alex-crist.com>
2024-07-21 20:52:28 -0500

    fix error

--- apps/portfolio/src/app/api/hello/route.ts
+++ apps/portfolio/src/app/api/hello/route.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { HelloDTO } from '@alex/models/lib/dto/hello.dto';
 
-export function GET(req: NextApiRequest, res: NextApiResponse<HelloDTO>) {
+export function handler(req: NextApiRequest, res: NextApiResponse<HelloDTO>) {
   if (req.method === 'GET') {
     const theHello: HelloDTO = { msg: 'Hello, from API as JSON!' };
     res.status(200).json(theHello);
