b7a05779bf594b5160fa164b13dee7cd1ab2042c
Alex Crist <alex@alex-crist.com>
2024-07-21 21:06:02 -0500

    .

--- apps/portfolio/src/app/api/hello/route.ts
+++ apps/portfolio/src/app/api/hello/route.ts
@@ -1,7 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { HelloDTO } from '@alex/models/lib/dto/hello.dto';
 
-export function get(req: NextApiRequest, res: NextApiResponse<HelloDTO>) {
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<HelloDTO>
+) {
   const theHello: HelloDTO = { msg: 'Hello, from API as JSON!' };
   res.status(200).json(theHello);
 }
