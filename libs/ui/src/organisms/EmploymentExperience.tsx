bce810ba5b9cfdc1ffb009f08fe98eb5a335240b
Alex Crist <alex@alex-crist.com>
2024-09-01 01:10:15 -0500

    .

--- libs/ui/src/organisms/EmploymentExperience.tsx
+++ libs/ui/src/organisms/EmploymentExperience.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+import { Card, CardContent } from '../atoms/Card';
 import {
   Carousel,
   CarouselIndicator,
@@ -7,8 +9,27 @@ import {
   CarouselThumbsContainer,
   SliderMainItem,
 } from './../molecules/EmploymentCarousel';
+import EmploymentCard from '../molecules/EmploymentCard';
+
+import { EmploymentExperienceDTO } from '@alex/models/lib/dto/employment-experience.dto';
+import { cn } from '../utils';
+import { SpinnerIcon } from '../atoms/icons/Spinner';
 
 const CarouselIndicatorExample = () => {
+  const [employmentData, setEmploymentData] = useState<
+    EmploymentExperienceDTO[]
+  >([]);
+
+  useEffect(() => {
+    async function fetchEmploymentData() {
+      const url = '/api/employment-experience';
+      const response = await fetch(url, { method: 'GET', cache: 'no-store' });
+      const res = await response.json();
+      setEmploymentData(res);
+    }
+    fetchEmploymentData();
+  }, []);
+
   return (
     <Carousel className="my-5">
       <CarouselNext />
@@ -19,6 +40,26 @@ const CarouselIndicatorExample = () => {
             <SliderMainItem key={index} className="bg-transparent">
               <div className="outline outline-1 outline-border size-full flex items-center justify-center rounded-xl bg-background">
                 Slide {index + 1}
+                {employmentData.map(
+                  (item: EmploymentExperienceDTO, index: number) => (
+                    <div key={index} className="p-1 text-stone-200">
+                      <Card>
+                        <CardContent className="flex items-center h-full justify-center p-0">
+                          <EmploymentCard
+                            index={index}
+                            companyName={item.companyName}
+                            companyUrl={item.companyUrl}
+                            dateStarted={item.dateStarted}
+                            dateEnded={item.dateEnded}
+                            position={item.position}
+                            responsibilities={item.responsibilities}
+                            technologies={item.technologies}
+                          />
+                        </CardContent>
+                      </Card>
+                    </div>
+                  )
+                )}
               </div>
             </SliderMainItem>
           ))}
