99a6c4cc2664803befef68a612460150c83febda
Alex Crist <alex@alex-crist.com>
2024-09-15 11:07:52 -0500

    .

--- libs/ui/src/organisms/EmploymentExperience/EmploymentExperience2.tsx
+++ libs/ui/src/organisms/EmploymentExperience/EmploymentExperience2.tsx
@@ -30,7 +30,7 @@ const EmploymentExperience2: React.FC<PropType> = (props) => {
       <div className="embla__viewport" ref={emblaRef}>
         <div className="embla__container">
           {slides.map((index) => (
-            <div className="embla__slide bg-stone-700" key={index}>
+            <div className="embla__slide" key={index}>
               <div className="embla__slide__number">{index + 1}</div>
             </div>
           ))}
