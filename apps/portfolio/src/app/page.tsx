8d7bc21d517a57dcfeec7d2952d3707cb9c38bd7
Alex Crist <alex@alex-crist.com>
2024-09-18 00:29:10 -0500

    .

--- apps/portfolio/src/app/page.tsx
+++ apps/portfolio/src/app/page.tsx
@@ -11,7 +11,59 @@ import CarouselIndicatorExample from '@alex/ui/organisms/EmploymentExperience';
 export default function Page() {
   const OPTIONS: EmblaOptionsType = { loop: true };
   const SLIDE_COUNT = 5;
-  const SLIDES = Array.from(Array(SLIDE_COUNT).keys());
+  // const SLIDES = Array.from(Array(SLIDE_COUNT).keys());
+  const SLIDES = [
+    {
+      ordinal: 1,
+      companyName: 'Company 1',
+      companyUrl: 'https://www.company1.com',
+      dateStarted: 'Jan 2020',
+      dateEnded: 'Dec 2020',
+      position: 'Frontend Developer',
+      responsibilities: ['Developed the frontend', 'Implemented new features'],
+      technologies: ['React', 'TypeScript', 'Tailwind CSS'],
+    },
+    {
+      ordinal: 2,
+      companyName: 'Company 2',
+      companyUrl: 'https://www.company2.com',
+      dateStarted: 'Jan 2021',
+      dateEnded: 'Dec 2021',
+      position: 'Frontend Developer',
+      responsibilities: ['Developed the frontend', 'Implemented new features'],
+      technologies: ['React', 'TypeScript', 'Tailwind CSS'],
+    },
+    {
+      ordinal: 3,
+      companyName: 'Company 3',
+      companyUrl: 'https://www.company3.com',
+      dateStarted: 'Jan 2022',
+      dateEnded: 'Dec 2022',
+      position: 'Frontend Developer',
+      responsibilities: ['Developed the frontend', 'Implemented new features'],
+      technologies: ['React', 'TypeScript', 'Tailwind CSS'],
+    },
+    {
+      ordinal: 4,
+      companyName: 'Company 4',
+      companyUrl: 'https://www.company4.com',
+      dateStarted: 'Jan 2023',
+      dateEnded: 'Dec 2023',
+      position: 'Frontend Developer',
+      responsibilities: ['Developed the frontend', 'Implemented new features'],
+      technologies: ['React', 'TypeScript', 'Tailwind CSS'],
+    },
+    {
+      ordinal: 5,
+      companyName: 'Company 5',
+      companyUrl: 'https://www.company5.com',
+      dateStarted: 'Jan 2024',
+      dateEnded: 'Dec 2024',
+      position: 'Frontend Developer',
+      responsibilities: ['Developed the frontend', 'Implemented new features'],
+      technologies: ['React', 'TypeScript', 'Tailwind CSS'],
+    },
+  ];
 
   const router = useRouter();
   const nav = (route: string) => {
