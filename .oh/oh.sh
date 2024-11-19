2f470389db5a3845d7e4bbf7651b60864d163c66
Alex Crist <alex@alex-crist.com>
2024-11-19 01:30:05 -0600

    .

--- .oh/oh.sh
+++ .oh/oh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export WORKSPACE_BASE=~/Code/gh/alex-crist-portfolio
-export CONTAINER_NAME=openhands_alex-crist-portfolio-$(date +%Y%m%d%H%M%S)
+export WORKSPACE_BASE=~/Code/gh/alex-crist-com-lov
+export CONTAINER_NAME=openhands_alex-crist-com-lov-$(date +%Y%m%d%H%M%S)
 
 mkdir -p $WORKSPACE_BASE
 
