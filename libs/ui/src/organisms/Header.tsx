5c80b92fcd66787a011e94cd70a691aeaca97f52
Alex Crist <alex@alex-crist.com>
2024-12-22 10:44:54 -0600

    .

--- libs/ui/src/organisms/Header.tsx
+++ libs/ui/src/organisms/Header.tsx
@@ -45,18 +45,18 @@ export function Header(props: HeaderProps) {
           <div className="h-[44px] flex flex-col justify-start align-start text-left">
             <Button
               variant="ghost"
-              className="text-[1.5rem] h-[20px] text-emerald-500 hover:text-emerald-400 tracking-wide font-serif font-semibold p-0 pr-2 ml-0 justify-start w-fit relative top-[2px]"
+              className="h-[20px] p-0 pr-2 ml-0 justify-start w-fit relative top-[2px]"
               onClick={() => nav('/')}
             >
               <span
                 title="Alex Crist Home page"
-                className="bg-gradient-to-r from-emerald-400 from-10% via-emerald-400 via-40% to-emerald-500 to-90% bg-clip-text text-stone-500/0"
+                className="text-[1.5rem] font-serif font-semibold tracking-wide bg-gradient-to-r from-emerald-500 from-10% via-emerald-400 via-40% to-emerald-500 to-90% bg-clip-text text-stone-500/0 hover:from-emerald-400 hover:via-emerald-300 hover:to-emerald-400"
               >
-                Alex Crist
+                Alex<span className="inline-block w-[4px]"></span>Crist
               </span>
             </Button>
-            <div className="block h-[24px] text-[.85rem] font-semibold text-stone-400 tracking-normal relative top-[2px]">
-              <span>Fullstack Extraordinaire</span>
+            <div className="block h-[24px] text-[.85rem] font-semibold text-stone-300 tracking-normal relative top-[2px]">
+              Fullstack Extraordinaire
             </div>
           </div>
         </div>
